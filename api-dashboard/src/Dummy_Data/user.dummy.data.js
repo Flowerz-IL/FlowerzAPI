908bfc40d8aff13de37723ceb3f412a916b544f4
nudel <guy.nudelman@aol.com>
2020-12-31 13:55:34 +0200

    creating dummy data

---
+++ api-dashboard/src/Dummy_Data/user.dummy.data.js
@@ -0,0 +1,24 @@
+const Users = [
+    { 
+        _id=1,
+        emailAddress:"Guy.nudelman@aol.com",
+        userRole:'USER',
+        firstName:"Guy",
+        surName:"Nudelman",
+        phoneNumber:"054-3399-082",
+        addresses:[{
+                name:"Home",
+                city:"Netaim",
+                street:"Hail",
+                houseNumber:"3",
+                floorNumber:0,
+                aptNumber:0
+            }],
+        cart:[{
+            orders:["test"],
+            totalSum:100
+        }]
+    },
+    
+
+]
